d22696f044f8ec7a76c7ef41dae5bf1021413c00
sarvesh-official <sarveshjaiofficial@gmail.com>
2023-10-20 22:19:37 +0530

    added

--- src/app.js
+++ src/app.js
@@ -1,59 +1,74 @@
 const PLAYERS = [
-    "Spiderman",
-    "Captain America",
-    "Wonderwoman",
-    "Popcorn",
-    "Gemwoman",
-    "Bolt",
-    "Antwoman",
-    "Mask",
-    "Tiger",
-    "Captain",
-    "Catwoman",
-    "Fish",
-    "Hulk",
-    "Ninja",
-    "Black Cat",
-    "Volverine",
-    "Thor",
-    "Slayer",
-    "Vader",
-    "Slingo"
+  "Spiderman",
+  "Captain America",
+  "Wonderwoman",
+  "Popcorn",
+  "Gemwoman",
+  "Bolt",
+  "Antwoman",
+  "Mask",
+  "Tiger",
+  "Captain",
+  "Catwoman",
+  "Fish",
+  "Hulk",
+  "Ninja",
+  "Black Cat",
+  "Volverine",
+  "Thor",
+  "Slayer",
+  "Vader",
+  "Slingo",
 ];
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
+  let detailedPlayers = players.map((player, index) => {
+    return {
+      name: player,
+      image: `images/super-${index + 1}.png`,
+      strength: getRandomStrength(),
+      type: getRandom(),
+    };
+  });
 
-    // Instead of forloop use Map method
-    // Code here
+  return detailedPlayers;
+};
 
-    return detailedPlayers;
+var type = ["hero", "villain"];
+function getRandom() {
+  return type[Math.floor(Math.random() * type.length)];
 }
 
 // getting random strength
 const getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100);
-}
+  return Math.ceil(Math.random() * 100);
+};
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
+  const filteredPlayers = players.filter((player) => player.type === type);
+  const playerTemplates = filteredPlayers.map((player) => {
+    return `<div class="player">
+              <img src="${player.image}">
+              <div class="name">${player.name}</div>
+              <div class="strength">${player.strength}</div>
+            </div>`;
+  });
 
-    // Instead of using for loop
-    // Use chaining of Array methods - filter, map and join
-    // Type your code here
-
-    return fragment;
-}
+  const fragment = playerTemplates.join("");
+  return fragment;
+};
 
 // Display players in HTML
 const viewPlayers = (players) => {
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
-}
-
+  document.getElementById("heroes").innerHTML = buildPlayers(players, "hero");
+  document.getElementById("villains").innerHTML = buildPlayers(
+    players,
+    "villain"
+  );
+};
 
 window.onload = () => {
-    viewPlayers(initPlayers(PLAYERS));
-}
+  viewPlayers(initPlayers(PLAYERS));
+};
